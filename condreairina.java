9f7fcdce305e89e8d61d9d08701c8569f2616ac9
condreairina <59140907+condreairina@users.noreply.github.com>
2019-12-22 22:25:22 +0200

    Add files via upload

---
+++ condreairina.java
@@ -0,0 +1,19 @@
+package irinabrad;
+public class condreairina {	
+	    public static void main(String[] args) {	    	
+long n=9;
+long s=99;
+long a=9999;
+long g=99999;
+long j=9;
+String ch="MERRY CHRISTMAS";
+
+System.out.printf("%5d%n",n);
+System.out.printf("%6%n",s);
+System.out.printf("%7%n",a);
+System.out.printf("%8%n",g);
+System.out.printf("%9%n",j);
+
+
+	    }
+}
